f81a74ed5b5f8ac0795d4548f58acde19057546c
MarcinRyba <marcin.rybinski95@gmail.com>
2022-08-07 10:01:46 +0200

    vault backup: 2022-08-07 10:01:46

--- review_tags.sh
+++ review_tags.sh
@@ -9,7 +9,8 @@ set -e
 oldIFS=$IFS
 action=$1
 folder_path=$2
-review_tag=" #review"
+review_tag1=" #review"
+review_tag2="#review"
 
 all_notes_to_review=$(find $folder_path -type f -printf "%p;")
 
@@ -18,11 +19,13 @@ for note_path in $all_notes_to_review
 do
     if [ $action == "add" ]
     then
-        sed -e s/$review_tag//g -i $note_path
-        echo -n $review_tag >> $note_path
+        sed -e s/$review_tag1//g -i $note_path
+        sed -e s/$review_tag2//g -i $note_path
+        echo -n $review_tag1 >> $note_path
     elif [ $action == "remove" ] 
     then
-        sed -e s/$review_tag//g -i $note_path
+        sed -e s/$review_tag1//g -i $note_path
+        sed -e s/$review_tag2//g -i $note_path
     fi
 done
 IFS=$oldIFS
